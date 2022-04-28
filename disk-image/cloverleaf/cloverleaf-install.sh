b192803cfec4b2d4c9cb41414ef1759ccb67741c
ECE666 Project <arndt20@purdue.edu>
2022-04-27 22:50:57 -0400

    updated cloverleaf packer files before build

--- disk-image/cloverleaf/cloverleaf-install.sh
+++ disk-image/cloverleaf/cloverleaf-install.sh
@@ -3,11 +3,5 @@
 #Compile SPEC hpc benchmark
 
 echo "12345" | sudo apt-get install build-essential gfortran
-echo "build essential done"
-
-# NOTE: we need to build
-# echo "building CloverLeaf OpenMP"
-# cd /home/gem5/CloverLeaf_OpenMP
-# make COMPILER=GNU MPI_COMPILER=gfortran C_MPI_COMPILER=gcc
 
 echo "Done Building SPEC hpc dependencies"
