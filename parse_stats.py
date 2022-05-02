8c76c9a8afa496819abc0d5f242f9dff96d87da0
ECE666 Project <arndt20@purdue.edu>
2022-05-01 23:28:22 -0400

    export parallel speedup plot to another file

--- parse_stats.py
+++ parse_stats.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 import os
+from plot_scripts.speedup import speedup
 
 
 def dprint(dict):
@@ -23,6 +24,9 @@ def parse(filename):
         "system.cache_hierarchy.ruby_system.L1Cache_Controller.M.Store::total": "private writes",
         "system.cache_hierarchy.ruby_system.L1Cache_Controller.E.Load::total": "private reads",
         "system.cache_hierarchy.ruby_system.L1Cache_Controller.S.Load::total": "shared reads",
+        "system.cache_hierarchy.ruby_system.L2Cache_Controller.L2_Replacement::total": "l2 stores",
+        "system.cache_hierarchy.ruby_system.L2Cache_Controller.Mem_Data::total": "l2 loads",
+        "system.cache_hierarchy.ruby_system.L2Cache_Controller.WB_Data::total": "l2 wb",
         # "system.cache_hierarchy.ruby_system.L2Cache_Controller.MT.L1_GETS::total": "MT L1_GETS",
         # "system.cache_hierarchy.ruby_system.L2Cache_Controller.MT.L1_PUTX::total": "L1_PUTX",
         # "system.cache_hierarchy.ruby_system.L2Cache_Controller.ISS.L1_GETS::total": "ISS L1_GETS",
@@ -56,103 +60,86 @@ def parse(filename):
 
 if __name__ == "__main__":
 
-    benchmarks = dict.fromkeys(["cloverleaf", "cloverleaf-copy"])
-
-    ps = dict.fromkeys([1, 2, 4, 8])
-
     if not os.path.isdir("parse_out"):
         print("Creating output directory")
         os.system("mkdir parse_out")
 
-    # parallel speed up graph
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    ps = [1, 2, 4, 8]
+    benchmarks = ["cloverleaf", "cloverleaf_copy"]
+    data = {}
 
     for b in benchmarks:
-        processors = []
-        speedups = []
-        ref_time = None
-
-        for p in sorted(ps):
+        data[b] = {}
+        for p in ps:
+            name = "{}-p{}-32kB".format(b, p)
             stats = parse(
-                "archive-remote/archive/{}-p{}-32kB-lock/stats.txt".format(b, p))
-            # dprint(stats)
-            if p == 1:
-                ref_time = stats["simSeconds"]
+                "archive-remote/archive/{}-lock/stats.txt".format(name))
+            data[b][name] = stats
 
-            processors.append(p)
-            speedups.append(ref_time / stats["simSeconds"])
+    speedup(data)  # Plot parallel speedup
 
-        ax.plot(processors, speedups, marker='o', label=b)
+    # # On Chip Traffic
+    # fig, ax = plt.subplots()
 
-    ax.set_ylabel('speedup')
-    ax.set_xlabel('processors')
-    ax.set_title('parallel speedup')
-    ax.legend()
-    plt.savefig('parse_out/speedup.png')
+    # processors = np.array([])
+    # s_writes = np.array([])
+    # p_writes = np.array([])
+    # s_reads = np.array([])
+    # p_reads = np.array([])
 
-    # On Chip Traffic
-    fig, ax = plt.subplots()
+    # for b in sorted(benchmarks):
+    #     for p in ["1", "2", "4", "8"]:
+    #         stats = parse(
+    #             "archive-remote/archive/{}-p{}-32kB-lock/stats.txt".format(b, p))
+    #         instr = 1
+    #         instr = stats["simInstr"] / 4  # TODO: UPDATE THIS FOR BYTES/INSTR
+    #         processors = np.append(processors, p)
+    #         s_writes = np.append(s_writes, stats["shared writes"]/instr)
+    #         s_reads = np.append(s_reads, stats["shared reads"]/instr)
+    #         p_writes = np.append(p_writes, stats["private writes"]/instr)
+    #         p_reads = np.append(p_reads, stats["private reads"]/instr)
 
-    processors = np.array([])
-    s_writes = np.array([])
-    p_writes = np.array([])
-    s_reads = np.array([])
-    p_reads = np.array([])
+    #     if b != sorted(benchmarks.keys())[-1]:
+    #         # Add spacing
+    #         processors = np.append(processors, "")
+    #         s_writes = np.append(s_writes, 0)
+    #         s_reads = np.append(s_reads, 0)
+    #         p_writes = np.append(p_writes, 0)
+    #         p_reads = np.append(p_reads, 0)
 
-    for b in sorted(benchmarks):
-        for p in ["1", "2", "4", "8"]:
-            stats = parse(
-                "archive-remote/archive/{}-p{}-32kB-lock/stats.txt".format(b, p))
-            instr = 1
-            instr = stats["simInstr"] / 4  # TODO: UPDATE THIS FOR BYTES/INSTR
-            processors = np.append(processors, p)
-            s_writes = np.append(s_writes, stats["shared writes"]/instr)
-            s_reads = np.append(s_reads, stats["shared reads"]/instr)
-            p_writes = np.append(p_writes, stats["private writes"]/instr)
-            p_reads = np.append(p_reads, stats["private reads"]/instr)
+    # width = 0.3
+    # x = np.arange(len(processors))  # the label locations
+    # ax.bar(x, p_reads, width, label='private reads')
+    # ax.bar(x, p_writes, width, label='private writes', bottom=p_reads)
+    # ax.bar(x, s_reads, width,
+    #        label='shared reads', bottom=p_reads+p_writes)
+    # ax.bar(x, s_writes, width, label='shared writes',
+    #        bottom=p_reads+p_writes+s_reads)
 
-        if b != sorted(benchmarks.keys())[-1]:
-            # Add spacing
-            processors = np.append(processors, "")
-            s_writes = np.append(s_writes, 0)
-            s_reads = np.append(s_reads, 0)
-            p_writes = np.append(p_writes, 0)
-            p_reads = np.append(p_reads, 0)
+    # handles, labels = ax.get_legend_handles_labels()
+    # ax.legend(handles[::-1], labels[::-1],
+    #           bbox_to_anchor=(1.04, 0.5), loc='upper left')
+    # ax.set_xticks(x, processors)
 
-    width = 0.3
-    x = np.arange(len(processors))  # the label locations
-    ax.bar(x, p_reads, width, label='private reads')
-    ax.bar(x, p_writes, width, label='private writes', bottom=p_reads)
-    ax.bar(x, s_reads, width,
-           label='shared reads', bottom=p_reads+p_writes)
-    ax.bar(x, s_writes, width, label='shared writes',
-           bottom=p_reads+p_writes+s_reads)
+    # ax.set_ylabel('Traffic (accesses)')
+    # ax.set_title('On-Chip Traffic')
 
-    handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles[::-1], labels[::-1],
-              bbox_to_anchor=(1.04, 0.5), loc='upper left')
-    ax.set_xticks(x, processors)
+    # # label the benchmarks
+    # # TODO: THIS IS HARDCORED TO TWO BENCHMARKS, MAYBE MAKE THIS FLEXIBLE FOR ANY NUMBER OF BENCHMARKS
+    # ax2 = ax.twiny()
 
-    ax.set_ylabel('Traffic (accesses)')
-    ax.set_title('On-Chip Traffic')
+    # ax2.spines["bottom"].set_position(("axes", -0.10))
+    # ax2.tick_params('both', length=0, width=0, which='minor')
+    # ax2.tick_params('both', direction='in', which='major')
+    # ax2.xaxis.set_ticks_position("bottom")
+    # ax2.xaxis.set_label_position("bottom")
 
-    # label the benchmarks
-    # TODO: THIS IS HARDCORED TO TWO BENCHMARKS, MAYBE MAKE THIS FLEXIBLE FOR ANY NUMBER OF BENCHMARKS
-    ax2 = ax.twiny()
+    # ax2.set_xticks([0.0, 0.5, 1.0])
+    # ax2.xaxis.set_major_formatter(ticker.NullFormatter())
+    # ax2.xaxis.set_minor_locator(ticker.FixedLocator([0.25, 0.75]))
+    # ax2.xaxis.set_minor_formatter(
+    #     ticker.FixedFormatter(sorted(benchmarks.keys())))
 
-    ax2.spines["bottom"].set_position(("axes", -0.10))
-    ax2.tick_params('both', length=0, width=0, which='minor')
-    ax2.tick_params('both', direction='in', which='major')
-    ax2.xaxis.set_ticks_position("bottom")
-    ax2.xaxis.set_label_position("bottom")
+    # fig.tight_layout()
 
-    ax2.set_xticks([0.0, 0.5, 1.0])
-    ax2.xaxis.set_major_formatter(ticker.NullFormatter())
-    ax2.xaxis.set_minor_locator(ticker.FixedLocator([0.25, 0.75]))
-    ax2.xaxis.set_minor_formatter(
-        ticker.FixedFormatter(sorted(benchmarks.keys())))
-
-    fig.tight_layout()
-
-    plt.savefig('parse_out/on-chip-traffic.png')
+    # plt.savefig('parse_out/on-chip-traffic.png')
---
+++ plot_scripts/speedup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import matplotlib.pyplot as plt
+
+
+def speedup(data):
+    '''
+    parallel speedup plot
+    '''
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    for b in data:
+        processors = []
+        speedups = []
+        ref_time = None
+        group = data[b]
+        for g in group:
+            params = g.split("-")
+            stats = group[g]
+            p = int(params[1][-1])
+            if p == 1:
+                ref_time = stats["simSeconds"]
+            processors.append(p)
+            speedups.append(ref_time / stats["simSeconds"])
+
+        ax.plot(processors, speedups, marker='o', label=b)
+
+    ax.set_ylabel('speedup')
+    ax.set_xlabel('processors')
+    ax.set_title('parallel speedup')
+    ax.legend()
+    plt.savefig('parse_out/speedup.png')
